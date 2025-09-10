e27224d8b7f1f30002d0784e41f7020bc470e9b8
Pyro Nicolini <178456562+pyro-nicolini@users.noreply.github.com>
2025-09-10 03:09:40 -0300

    feat: add card, button1 and button2

--- src/vistas/publico/Home.jsx
+++ src/vistas/publico/Home.jsx
@@ -1,8 +1,14 @@
 function Home() {
   return (
     <div>
-      <h1>hola mundo</h1>
-      <button className="btn-primary">Click Me</button>
+      <h1 className="text-gradient">Hola Mundo</h1>
+
+    <div className="card m-1">
+      <h2>Esta es una Card</h2>
+      <p>con margin 1rem  =  m-1</p>
+      <button className="btn btn-primary">Click Me</button>
+      <button className="btn btn-secondary">Click Me</button>
+    </div>
     </div>
   );
 }
